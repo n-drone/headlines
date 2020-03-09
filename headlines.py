899455fe0b0aa741d02c4f8d9504df5c2cadf102
Some Name <ewigeis@gmail.com>
2020-03-10 01:04:28 +0300

    show headlines from BBC and lenta. URLs: <sitename>/bbc (for BBC), <sitename>/lenta (for lenta)

--- headlines.py
+++ headlines.py
@@ -1,26 +1,33 @@
 import feedparser
 from flask import Flask
 app = Flask(__name__)
-BBC_FEED = "http://feeds.bbci.co.uk/news/england/london/rss.xml"
+RSS_FEED = {'bbc'   : 'http://feeds.bbci.co.uk/news/england/london/rss.xml',
+            'lenta' : 'http://lenta.ru/rss/news' }
+
+def get_news(source):
+    feed = feedparser.parse(RSS_FEED[source])
+    articles = feed['entries']
+    html = "<html><body>"
+    html += "<h1>{} Headlines</h1>".format(source)
+    for article in articles:
+       html += """ 
+            <b>{0}</b> <br/>
+            <i>{1}</i> <br/>
+            <p>{2}</p> <br/>
+            """.format(article.get("title"),
+                       article.get("published"),
+                       article.get("summary"))
+    html += "</body></html>"
+    return html
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
-    first_article = feed['entries'][0]
-    html = "<html>"
-    html += "<body>"
-    html += "<h1>BBC Headlines</h1>"
-    for article in feed['entries']:
-        html += """<b>{0}</b> </br>
-                    <i>{1}</i> </br>
-                    <p>{2}</p> </br>""".format(
-            article.get("title"),
-            article.get("published"),
-            article.get('summary'))
-    html += "</body>"
-    html += "</html>"
+@app.route("/bbc")
+def get_bbc():
+    return get_news('bbc')
 
-    return html
+@app.route("/lenta")
+def get_lenta():
+    return get_news('lenta')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
